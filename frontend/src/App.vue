6c207ccb53690206ef8247635e7fd08004275390
Harut <harut9999999@gmail.com>
2022-09-15 13:26:13 +0400

    make frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <LoginComponent msg="Welcome to Your Vue.js App" />
+</div>
+</template>
+
+<script>
+import LoginComponent from './components/LoginComponent'
+
+export default {
+    name: 'App',
+    components: {
+      LoginComponent
+    }
+}
+</script>
+
+<style>
+  *,
+*::before,
+*::after {
+  margin: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  cursor: pointer;
+}
+button {
+  background-color: transparent;
+  color: inherit;
+  border-width: 0;
+  padding: 0;
+  cursor: pointer;
+}
+figure {
+  margin: 0;
+}
+ul,
+dd {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  margin: 0;
+  font-size: inherit;
+  font-weight: inherit;
+}
+p {
+  margin: 0;
+}
+</style>
